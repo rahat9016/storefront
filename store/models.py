9680387f8c92273f720cd177674be61d7214fd23
minhajurrohoman9016 <minhajurrohoman9016@gmail.com>
2024-06-07 16:50:38 +0600

    created likes model

--- store/models.py
+++ store/models.py
@@ -6,7 +6,7 @@ class Promotion(models.Model):
     discount = models.FloatField()
 class Collection(models.Model):
     title = models.CharField(max_length=255)
-    feature_product = models.ForeignKey('Product', on_delete=models.SET_NULL, null=True, related_name='+')
+    feature_product = models.ForeignKey('store.Product', on_delete=models.SET_NULL, null=True, related_name='+')
 
 class Product(models.Model):
     title = models.CharField(max_length=255)
